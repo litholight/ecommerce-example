40a674f759c9d262b8eb02e3361d7319dae75790
Stoneburner Software <sean@stoneburnersoftware.com>
2019-08-27 14:12:30 -0400

    finished signin form and added to signin page

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -12,25 +12,46 @@ class SignIn extends React.Component {
     };
   }
 
-  render() (
-    <div className="sign-in">
-      <h2>I already have an account</h2>
-      <span>Sign in with your email and password</span>
+  handleSubmit = event => {
+    event.preventDefault();
 
-      <form>
-        <input name="email" type="email" value={this.state.email} required />
-        <label>Email</label>
-        <input
-          name="password"
-          type="password"
-          value={this.state.password}
-          required
-        />
-        <label>Password </label>
-        <input type="submit" value="Submit Form" />
-      </form>
-    </div>;
-  )
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="email"
+            type="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+          <label>Email</label>
+          <input
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+          <label>Password </label>
+          <input type="submit" value="Submit Form" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
