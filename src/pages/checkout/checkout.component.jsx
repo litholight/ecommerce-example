16a648e1a7ddc3a805614f6168bbedd3b2408340
Stoneburner Software <sean@stoneburnersoftware.com>
2019-09-08 12:40:36 -0400

    created checkout page with memoized cart-total selector

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import "./checkout.styles.scss";
+import { createStructuredSelector } from "reselect";
+import {
+  selectCartItems,
+  selectCartHidden,
+  selectCartTotal
+} from "../../redux/cart/cart.selectors";
 
-const CheckOutPage = () => <div>CHECKOUT PAGE</div>;
+const CheckOutPage = ({ cartItems, total }) => (
+  <div className="checkout-page">
+    <div className="checkout-header">
+      <div className="header-block">
+        <span>Product</span>
+      </div>
+      <div className="header-block">
+        <span>Description</span>
+      </div>
+      <div className="header-block">
+        <span>Quantity</span>
+      </div>
+      <div className="header-block">
+        <span>Price</span>
+      </div>
+      <div className="header-block">
+        <span>Remove</span>
+      </div>
+    </div>
+    {cartItems.map(cartItem => cartItem.name)}
+    <div className="total">
+      <span>TOTAL: ${total}</span>
+    </div>
+  </div>
+);
 
-export default CheckOutPage;
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectCartTotal
+});
+
+export default connect(mapStateToProps)(CheckOutPage);
