a7236eed55d54014c2dc9f1aeea0ebc217bc398d
Stoneburner Software <sean@stoneburnersoftware.com>
2019-09-15 07:57:31 -0400

    changed logo and data with books

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,36 +1,16 @@
 const INITIAL_STATE = {
   sections: [
     {
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      title: "Philosophy of Science",
+      imageUrl: "/book-images/philoscience/philoScienceCentral.jpg",
       id: 1,
-      linkUrl: "shop/hats"
+      linkUrl: "shop/philoscience"
     },
     {
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      title: "Old Testament",
+      imageUrl: "/book-images/oldtestament/ancientisraelitereligion.jpg",
       id: 2,
-      linkUrl: "shop/jackets"
-    },
-    {
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-      id: 3,
-      linkUrl: "shop/sneakers"
-    },
-    {
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-      size: "large",
-      id: 4,
-      linkUrl: "shop/womens"
-    },
-    {
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-      size: "large",
-      id: 5,
-      linkUrl: "shop/mens"
+      linkUrl: "shop/oldtestament"
     }
   ]
 };
